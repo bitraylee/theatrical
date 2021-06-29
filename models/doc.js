93d529249e11ccc2dc70fc600526facb0afa1157
bitraylee <anshumohanta@hotmail.com>
2021-06-29 12:40:02 +0530

    fixing folder struc.

--- models/doc.js
+++
@@ -1,7 +0,0 @@
-// just to display endpoints
-exports.documentation = {
-    display_all_tasks: "/todo/all",
-    display_completed_tasks: "/todo/finished",
-    display_unfinished_tasks: "/todo/unfinished",
-    display_specific_tasks: "/todo/<taskid>"
-}
