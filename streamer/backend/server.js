0eef8134f1a6de2b8bd81c854f0d9af8d906e176
souvikinator <souvikinator@pop-os.localdomain>
2021-06-29 18:28:31 +0530

    added backend code

--- streamer/backend/server.js
+++ streamer/backend/server.js
@@ -1,25 +1,42 @@
-// dotenv for easily accessing environment variables
-// keep it at the top!
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const todoRoute=require('./routes/todo');
-const cors=require('cors');
-const doc=require('./models/doc');
+const cors = require("cors");
+var randomstring = require("randomstring");
 
 // set port number
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3100;
 // to allow cross-origin requests
 app.use(cors());
 
-// api endpoint accessible from /todo/...
-app.use("/todo",todoRoute);
+let video_src;
+let rooms = {};
 
-app.get('/',(req,res)=>{
-    res.status(200).send(doc);
+// TODO: change it to post request
+app.get("/createroom", (req, res) => {
+  let roomid;
+  while (1) {
+    roomid = randomstring.generate(8);
+    if (!rooms.hasOwnProperty(roomid)) break;
+  }
+  video_src = req.body.vsrc;
+  rooms[roomid] = { vrsc: video_src };
+  console.log({ roomid, video_src });
+  res.redirect(`/room/${roomid}?vs=${video_src}`);
+});
+
+app.get("/room/:id", (req, res) => {
+  let id = req.params.id || null;
+  if (!rooms.hasOwnProperty(id)) {
+    console.log("redirect");
+    // redirect to search page
+    res.redirect("http://locahost:3000/");
+    return;
+  }
+  res.status(200).send({ vsrc: video_src, room: id });
 });
 
 // start server
 app.listen(PORT, () => {
-    console.log(`server running at >${PORT}<`)
+  console.log(`server running at >${PORT}<`);
 });
